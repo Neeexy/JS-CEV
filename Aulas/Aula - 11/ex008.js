34ecf2ec077cb270e67a0d01d572e0c28095db36
Neeexy <nexy.2809@gmail.com>
2024-10-05 15:03:22 -0300

    a

---
+++ Aulas/Aula - 11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+console.log('Dirija sempre usando cinto de segurança.')
+if (vel > 60){
+console.log('Você ultrapassou a velocidade permitida. MULTADO')}
