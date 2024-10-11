e35704f741ce077663da05a2abeb478b63fd0d66
Neeexy <nexy.2809@gmail.com>
2024-10-11 20:45:12 -0300

    a

--- Exercícios/ex005/script.js
+++ Exercícios/ex005/script.js
@@ -1,18 +1,6 @@
 function tabuada(){
-    let num = Number(document.querySelector('#num').value)
-    let tab = document.querySelector('#seltab')
-
-    if (num == 0){
-        alert('Por favor, digite um número!')
-    }
-
-    let c = 0
-    tab.innerHTML = ''
-
-    while(c <=10){
-        let item = document.createElement('option')
-        item.text = `${num} x ${c} = ${num*c}`
-        tab.appendChild(item)
-        c +=1
-    }
+    var resultado = document.querySelector('#res')
+    var foto = document.createElement('img')
+    foto.src = 'https://t4.ftcdn.net/jpg/04/81/25/75/360_F_481257597_wISemHsawXGjRUGbRLIDOZFOqEKSvnxL.jpg'
+    resultado.appendChild(foto)
 }
